e0c61eb66a7a632017940da0e2d5906d05db24f4
Manfred Walder <office@manfredwalder.at>
2016-04-18 10:20:13 +0200

    social app debug

--- resources/views/includes/status.blade.php
+++ resources/views/includes/status.blade.php
@@ -7,7 +7,7 @@
 
         type:"{{ Session::get('status') }}"
       });
-      @if(Session::has('response'))
+      @if(Session::has('user'))
       console.log({!! dd(Session::all()) !!})
       @endif
     </script>
