45bbcb52926efb34e74f3c040652f649b8a4b934
Manfred Walder <office@manfredwalder.at>
2016-04-18 16:40:47 +0200

    adding ga code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -46,7 +46,16 @@
     </div>
   </div>
     </div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+      ga('create', 'UA-27873180-9', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     @yield('scriptPluginsPre')
     <script src="http://cdn.leafletjs.com/leaflet/v1.0.0-beta.2/leaflet.js"></script>
     <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
