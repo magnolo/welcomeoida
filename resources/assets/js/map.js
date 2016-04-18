3067d77d350cc272e2f04fa1f419399aec8a993d
Manfred Walder <office@manfredwalder.at>
2016-04-18 12:19:10 +0200

    changed pin message

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -88,82 +88,6 @@ $(function() {
     }
   }).addTo($.datas.map);
 
-  // DRAW ICON BY DRAGGING ON MAP
-  // var drawnItems = new L.FeatureGroup();
-  // //$.datas.map.addLayer(drawnItems);
-  //
-  // // Initialise the draw control and pass it the FeatureGroup of editable layers
-  // var drawControl = new L.Control.Draw({
-  //     position:'topright',
-  //     edit: {
-  //         featureGroup: drawnItems,
-  //     },
-  //     draw:{
-  //       polyline:false,
-  //       polygon:false,
-  //       rectangle:false,
-  //       circle:false
-  //     }
-  // });
-  // drawControl.addTo($.datas.map);
-  // $.datas.map.on('draw:created', function (e) {
-  //   var type = e.layerType,
-  //       layer = e.layer;
-  //
-  //   if (type === 'marker') {
-  //       // Do marker specific actions
-  //       console.log('markt')
-  //   }
-  //
-  //   // Do whatever else you need to. (save to db, add to map etc)
-  //   $.datas.map.addLayer(layer);
-  // });
-
-
-  // LOCATE ME
-  // L.control.locate({
-  //   position: 'topright', // set the location of the control
-  //   layer: undefined, // use your own layer for the location marker, creates a new layer by default
-  //   drawCircle: true, // controls whether a circle is drawn that shows the uncertainty about the location
-  //   follow: true, // follow the user's location
-  //   setView: true, // automatically sets the map view to the user's location, enabled if `follow` is true
-  //   keepCurrentZoomLevel: false, // keep the current map zoom level when displaying the user's location. (if `false`, use maxZoom)
-  //   stopFollowingOnDrag: false, // stop following when the map is dragged if `follow` is true (deprecated, see below)
-  //   remainActive: false, // if true locate control remains active on click even if the user's location is in view.
-  //   markerClass: L.circleMarker, // L.circleMarker or L.marker
-  //   circleStyle: {
-  //     color: '#EB5B27',
-  //     opacity: 0.2,
-  //     fillColor: '#EB5B27'
-  //   }, // change the style of the circle around the user's location
-  //   markerStyle: {
-  //     color: '#EB5B27',
-  //     opacity: 1,
-  //     fillColor: '#EB5B27'
-  //   },
-  //   followCircleStyle: {}, // set difference for the style of the circle around the user's location while following
-  //   followMarkerStyle: {},
-  //   icon: 'fa fa-map-marker', // class for icon, fa-location-arrow or fa-map-marker
-  //   iconLoading: 'fa fa-spinner fa-spin', // class for loading icon
-  //   iconElementTag: 'span', // tag for the icon element, span or i
-  //   circlePadding: [0, 0], // padding around accuracy circle, value is passed to setBounds
-  //   metric: true, // use metric or imperial units
-  //   onLocationError: function(err) {
-  //     alert(err.message)
-  //   }, // define an error callback function
-  //   onLocationOutsideMapBounds: function(context) { // called when outside map boundaries
-  //     alert(context.options.strings.outsideMapBoundsMsg);
-  //   },
-  //   showPopup: true, // display a popup when the user click on the inner marker
-  //   strings: {
-  //     title: "Zeige mir wo ich bin", // title of the locate control
-  //     metersUnit: "metern", // string for metric units
-  //     feetUnit: "feet", // string for imperial units
-  //     popup: "Du befindest dich im Umkreis von {distance} {unit} zu diesem Punkt", // text to appear if user clicks on circle
-  //     outsideMapBoundsMsg: "Du befindest dich außerhalb der Grenzen dieser Karte!?" // default message for onLocationOutsideMapBounds
-  //   },
-  //   locateOptions: {} // define location options e.g enableHighAccuracy: true or maxZoom: 10
-  // }).addTo($.datas.map);
 
   $.datas.layer = L.Util.ajax("/api/pois/all").then(function(data) {
     $.datas.layers.markers = L.geoJson.css(data);
@@ -206,7 +130,7 @@ $(function() {
       $('#solidarisch .btn').attr('disabled', true);
       $.post('/api/pois/humans', newHuman, function(response) {
         swal({
-          title: "Baam!",
+          title: "Danke für dein WELCOMEoida!",
           text: "Dein Pin wurde auf die Karte gesetzt!",
           type: "success",
           confirmButtonText: "Ok!",
