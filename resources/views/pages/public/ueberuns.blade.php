206ef22a696957a6008a85e40dc0eb5b99aee84c
Manfred Walder <office@manfredwalder.at>
2016-04-18 12:35:50 +0200

    newhere link correction

--- resources/views/pages/public/ueberuns.blade.php
+++ resources/views/pages/public/ueberuns.blade.php
@@ -7,88 +7,88 @@
     <div class="container">
 
       <div class="row">
-	    
+
         <div class="col s12 center-align navtwo">
 			<a href="https://www.facebook.com/events/1140455429322099/" class="button">Join us on Facebook</a>
 			<a href="https://twitter.com/welcomeoida_at" class="button">Follow us on Twitter</a>
 			<a href="mailto:infos@welcomeoida.at" class="button">E-Mail us!</a>
 			<a href="http://test.welcomeoida.at/aktiv-werden" class="button">Get involved!</a>
-        </div>	    
-	    
+        </div>
+
         <div class="col s12 center-align contentheader">
 	        <h2>Über uns</h2>
-			<p>Ein Wiener Plädoyer für die Fortsetzung der Willkommenskultur.</p>	        
+			<p>Ein Wiener Plädoyer für die Fortsetzung der Willkommenskultur.</p>
         </div>
       </div>
 
     <!-- Hier ist der HauptContent -->
 
-      <div class="row">        
+      <div class="row">
 	      <div class="col m12 s12">
-   		      <div class="graybox">		      
+   		      <div class="graybox">
 					<h2>Wir sind New Here. und sagen "WELCOMEoida"</h2>
-					
+
 						<p>
 							Seit dem vergangenen Sommer trägt die Zivilgesellschaft in Wien entscheidend zur Versorgung und Betreuung von geflüchteten Menschen bei.
 							Damit hat sie wesentlichen Anteil daran, dass Schutzsuchende eine menschenwürdige Aufnahme in unserer Stadt finden.
 						</p>
-						
+
 						<p>
-							Zuletzt mehren sich jedoch politische Stimmen, die für ein Ende der ‚Willkommenskultur‘ plädieren, Sozialleistungen für Geflüchtete kürzen wollen, 
+							Zuletzt mehren sich jedoch politische Stimmen, die für ein Ende der ‚Willkommenskultur‘ plädieren, Sozialleistungen für Geflüchtete kürzen wollen,
 							'Obergrenzen' für Asylanträge und befestigte Grenzzäune fordern und behaupten, die Solidarität in der Bevölkerung sei Schnee von gestern.
 						</p>
-						
+
 						<p>
-							Die Realität zeichnet allerdings ein ganz anderes Bild. Zahllose Flüchtlinge werden nach wie vor von Privatpersonen versorgt und betreut, 
+							Die Realität zeichnet allerdings ein ganz anderes Bild. Zahllose Flüchtlinge werden nach wie vor von Privatpersonen versorgt und betreut,
 							zivilgesellschaftliche Initiativen unterstützen Flüchtlinge weiterhin dabei, in ihrem neuen Leben Fuß zu fassen.
-							Es wird gekocht, übersetzt, Deutsch gelernt, Behördengänge werden erledigt, es wird gebastelt, organisiert und gemeinsam an Lösungen für die kleinen 
+							Es wird gekocht, übersetzt, Deutsch gelernt, Behördengänge werden erledigt, es wird gebastelt, organisiert und gemeinsam an Lösungen für die kleinen
 							und großen Fragen des Alltags gearbeitet.<br />
 							<br />
 							<strong>Und jeden Tag zeigt Wien auf’s Neue: Wir schaffen das!</strong>
 						</p>
-					
+
 						<p>
-							Unser Beitrag dazu ist der interaktive, mehrsprachige Stadtplan <strong>New Here</strong>, der geflüchteten Menschen einen Überblick über Angebote in der 
+							Unser Beitrag dazu ist der interaktive, mehrsprachige Stadtplan <strong>New Here</strong>, der geflüchteten Menschen einen Überblick über Angebote in der
 							Stadt bietet und sie dabei unterstützt, sich selbstbestimmt in Wien zu orientieren.<br /><br />
-							Erfahre mehr unter: <a href="newhere.html" class="button">New Here.</a><br /><br />
+							Erfahre mehr unter: <a href="/newhere" class="button">New Here.</a><br /><br />
 						</p>
-					
+
 						<p>
-							Die <strong>Karte wird am 20. Juni, dem Weltflüchtlingstag, online gehen.</strong> Wir wollen am Tag des Kartenlaunchs - besonders anlässlich der aktuellen 
-							politischen Stimmung - sichtbar machen, dass unsere Stadt Flüchtlinge nach wie vor willkommen heißt und wir, die Zivilgesellschaft in Wien, weiterhin solidarisch 
+							Die <strong>Karte wird am 20. Juni, dem Weltflüchtlingstag, online gehen.</strong> Wir wollen am Tag des Kartenlaunchs - besonders anlässlich der aktuellen
+							politischen Stimmung - sichtbar machen, dass unsere Stadt Flüchtlinge nach wie vor willkommen heißt und wir, die Zivilgesellschaft in Wien, weiterhin solidarisch
 							mit schutzsuchenden Menschen sind.
 						</p>
-					
+
 						<p>
-							Wir laden ein sich dem Motto "#WELCOMEoida: In unserer Stadt sind Flüchtlinge willkommen. - Wien bleibt solidarisch!" anzuschließen und gemeinsam ein interaktives 
-							Bild der zivilgesellschaftlichen Solidarität zu zeichnen: Einzelpersonen, 
-							Kultureinrichtungen, Kultur- und Sportvereine, NGOs, Gewerkschaften, StudentInnenorganisationen, Restaurants, Kinos, Beisl, Bars, Würstel- und 
-							KebabstandbetreiberInnen, Gallerien und Museen. Dabei können Konzerte, Parties, Performances, Soli-Feste, Kinovorführungen, Theaterstücke, Lesungen, 
+							Wir laden ein sich dem Motto "#WELCOMEoida: In unserer Stadt sind Flüchtlinge willkommen. - Wien bleibt solidarisch!" anzuschließen und gemeinsam ein interaktives
+							Bild der zivilgesellschaftlichen Solidarität zu zeichnen: Einzelpersonen,
+							Kultureinrichtungen, Kultur- und Sportvereine, NGOs, Gewerkschaften, StudentInnenorganisationen, Restaurants, Kinos, Beisl, Bars, Würstel- und
+							KebabstandbetreiberInnen, Gallerien und Museen. Dabei können Konzerte, Parties, Performances, Soli-Feste, Kinovorführungen, Theaterstücke, Lesungen,
 							das Ausschenken von Soligetränken, Grätzelfeste, Interventionen, etc. organisiert werden.
 						</p>
-					
+
 						<p>
-							Jede Aktion, die am 20. Juni stattfindet, kann auf auf der Karte eingetragen werden und scheint als Punkt „Bei uns sind Flüchtlinge willkommen“ auf. 
+							Jede Aktion, die am 20. Juni stattfindet, kann auf auf der Karte eingetragen werden und scheint als Punkt „Bei uns sind Flüchtlinge willkommen“ auf.
 							So entsteht ein interaktives Bild der zivilgesellschaftlichen Solidarität in Wien.
 						</p>
 		   		      <p style="text-align: center;"><br />
 			   		      <img src="http://www.craftandvalue.com/_welcomeOida/downloads/bild8.jpg" />
 		   		      </p>
-   		      </div>         		      
-	      </div>     	      
-      </div>	
-      
-      
-    <!-- Hier ist der Footer --> 
-      <div class="row">        
+   		      </div>
+	      </div>
+      </div>
+
+
+    <!-- Hier ist der Footer -->
+      <div class="row">
 	      <div class="col m12 s12">
 		      <div class="copyright">
 			      &copy;2016 - NewHere.
 		      </div>
 	      </div>
       </div>
-      
-      
+
+
 @stop
 @section('scripts')
     <!-- Hier ist Platz für javascript-->
