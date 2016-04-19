e49929456affe03743f26ce49206de7b7ed5a132
Manfred Walder <office@manfredwalder.at>
2016-04-19 09:36:14 +0200

    spelling and link errors

--- resources/views/pages/public/aktiv-werden.blade.php
+++ resources/views/pages/public/aktiv-werden.blade.php
@@ -8,94 +8,94 @@
     <div class="container">
 
       <div class="row">
-	    
+
         <div class="col s12 center-align navtwo">
 			<a href="https://www.facebook.com/events/1140455429322099/" class="button">Join us on Facebook</a>
 			<a href="https://twitter.com/welcomeoida_at" class="button">Follow us on Twitter</a>
 			<a href="mailto:info@welcomeoida.at" class="button">E-Mail us!</a>
 			<a href="http://www.welcomeoida.at/aktiv-werden" class="button">Get involved!</a>
-        </div>	    
-	    
+        </div>
+
         <div class="col s12 center-align contentheader">
 	        <h2>Aktiv werden!</h2>
-			<p>Wir freuen uns über viele Menschen die sich einbringen wollen.</p>	        
+			<p>Wir freuen uns über viele Menschen die sich einbringen wollen.</p>
         </div>
       </div>
 
 
     <!-- Hier ist der HauptContent -->
 
-      <div class="row">        
+      <div class="row">
 	      <div class="col m4 s12">
 		      <div class="graybox">
 			      <h3>Mach mit bei #WELCOMEoida!</h3>
-   				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/ballonPattern.png" style="border-radius: 400px; margin: 0 0 15px 0;" />			      
+   				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/ballonPattern.png" style="border-radius: 400px; margin: 0 0 15px 0;" />
 				  <p>Wir wollen zeigen dass sehr, sehr viele WienerInnen solidarisch mit geflüchteten Menschen sind. Jeder Eintrag hilft das breite Engagement in unserer Stadt abzubilden. Hier kannst du dich registrieren.</p>
-   				  <a href="https://www.welcomeoida.at" class="button">In die Karte eintragen!</a> 
+   				  <a href="http://www.welcomeoida.at" class="button">In die Karte eintragen!</a>
 			   </div>
 		      <div class="graybox">
  			      <h3>Spann‘ deinen Schirm auf!</h3>
- 				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/umbrellaMarch.png" style="border-radius: 400px; width: 100%; margin: 0 0 25px 0;" /> 			      
+ 				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/umbrellaMarch.png" style="border-radius: 400px; width: 100%; margin: 0 0 25px 0;" />
  				  <p>Auch dieses Jahr findet am 20. Juni auch in Österreich der Europäische Umbrella March statt. Alle Infos dazu gibt es unter: <br /><br /><a href="http://www.asyl.at/" class="button">Asyl.at</a></p>
- 			  </div>				   
+ 			  </div>
 		      <div class="graybox">
  			      <h3>Gewinne MitstreiterInnen:</h3>
  				  Wir sind viele! Damit man das am 20. Juni auch sieht, sprich deine LieblingswirtInnen, WürtselstandInnhaberInnen, KinobetreiberInnen, TheatermacherInnen und wer dir sonst noch einfällt an, sich #WELCOMEoida anzuschliessen.
- 			  </div>			   
+ 			  </div>
 	      </div>
-	      
+
   	      <div class="col m4 s12">
 		      <div class="graybox">
  			      <h3>Werde Teil vom Team:</h3>
  				  <p>Wir sind ein buntes Team engagierter Menschen, die sich über Unterstützung freuen!</p>
- 				  <a href="mailto:info@welcomeoida.at" class="button">E-Mail us!</a> 				  
+ 				  <a href="mailto:info@welcomeoida.at" class="button">E-Mail us!</a>
  			  </div>
 		      <div class="graybox">
  			      <h3>Unterstütze uns finanziell:</h3>
- 				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/respektNet.png" style="border-radius: 400px; width: 100%; margin: 0 0 25px 0;" />  			      
+ 				  <img src="http://www.craftandvalue.com/_welcomeOida/downloads/respektNet.png" style="border-radius: 400px; width: 100%; margin: 0 0 25px 0;" />
  				  <p>Wir alle arbeiten ehrenamtlich und versuchen so Vieles wie möglich kostenlos zu bekommen. Für jene Dinge die  bezahlt werden müssen, brauchen wir eure Unterstützung!</p>
-   				<a href="http://www.respekt.net/projekte-unterstuetzen/details/projekt/1231/" class="button" />Crowdfunder Respekt.net</a>	
+   				<a href="http://www.respekt.net/projekte-unterstuetzen/details/projekt/1231/" class="button" />Crowdfunder Respekt.net</a>
 				</div>
 		      <div class="graybox">
  			      <h3>Macht Kunst!</h3>
 					Wir freuen uns über KünstlerInnen und Kulturschaffende, die bereit sind am 20. Juni ihr Können zur Verfügung zu stellen. Falls ihr auf der Suche nach PartnerInnen sind, helfen wir gerne weiter!<br /><br />
 					<a href="mailto:info@welcomeoida.at" class="button">E-Mail us!</a>
-				</div> 						  
+				</div>
 	      </div>
-		  
+
 	      <div class="col m4 s12">
 		      <div class="graybox">
    			      <h3>Nutze Facebook um auf #WELCOMEoida aufmerksam zu machen</h3>
    				  <img src="http://menschenrechte-grenzenlos.eu/_images/_involved/facebooklogo.png" style="border-radius: 400px; margin: 0 0 25px 0;" /><br />
-   				  <a href="https://www.facebook.com/events/1140455429322099/" class="button">Join us on Facebook</a>   				  
+   				  <a href="https://www.facebook.com/events/1140455429322099/" class="button">Join us on Facebook</a>
    			  </div>
 		      <div class="graybox">
  			      <h3>Komm zu den Veranstaltungen am 20. Juni</h3>
  			      <p>Lade dir unsere Logo herunter und drucke es auf Plakate, Poster oder T-Shirts und nimm am Umbrellamarch am 20. Juni teil. Lass uns Solidarität zeigen, die wir mit den Menschen haben die in unserer Stadt Schutz suchen!</p>
-   				<a href="http://test.welcomeoida.at/downloads" class="button" />Downloads</a>	 			      
+   				<a href="http://www.welcomeoida.at/downloads" class="button" />Downloads</a>	 			      
  			  </div>
 		      <div class="graybox">
  			      <h3>Sagt als Organisation #WELCOMEoida!</h3>
  			      <p>Auch wenn ihr am 20. Juni kein spezielles Projekt oder Event plant, könnt ihr als Organisation eure Solidarität bekunden.</p>
- 			  </div>   			  			  
+ 			  </div>
 	      </div>
-	      
+
       </div>
 
       </div>
     </div>
-    
-    
-    <!-- Hier ist der Footer --> 
-      <div class="row">        
+
+
+    <!-- Hier ist der Footer -->
+      <div class="row">
 	      <div class="col m12 s12">
 		      <div class="copyright">
 			      &copy;2016 - NewHere.
 		      </div>
 	      </div>
-      </div>    
-    
-    
+      </div>
+
+
 @stop
 @section('scripts')
     <!-- Hier ist Platz für javascript-->
